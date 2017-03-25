30849ec19984f7a66d093ab574199bb4a399b2c5
20kdc <asdd2808@gmail.com>
2017-03-25 11:36:21 +0000

    Fix inverted RS signal
    how do I make mistakes in a row like this???

--- oc/uc-rsctl.lua
+++ oc/uc-rsctl.lua
@@ -53,7 +53,7 @@ TS(t,g..S.char(a,1)..d)x=RP(j,R4)end
 RN[t..g]=x
 end j()end
 -- EXAMPLE IoT Redstone Top Controller --
-UR=CG("redstone")UM={["true"]=0,["false"]=15}function UT(p)if UM[p]then
+UR=CG("redstone")UM={["true"]=15,["false"]=0}function UT(p)if UM[p]then
 US=p
 UR.setOutput(1,UM[p])end
 end
